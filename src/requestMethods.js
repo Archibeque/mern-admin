68bf8022e0713a28d5b1bc6ea92f5f279f8fa573
nnadidan360 <nnadidan360@gmail.com>
2023-05-12 04:47:59 +0100

    removed error on request

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASEURL;
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken;
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser?.accessToken;
 
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+console.log(TOKEN)
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
